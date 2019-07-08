d1526445fd5f402288a5c8d5c11c4ffb152d94c8
ofirsomech <ofirsoemch@hotmail.com>
2019-07-08 12:18:33 +0300

    set all shows with cards

--- src/Screens/Test.js
+++ src/Screens/Test.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { Image } from 'react-native';
+import {
+  Container,
+  Header,
+  DeckSwiper,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  View,
+  Left,
+  Body,
+  Icon
+} from 'native-base';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import axios from 'axios';
+
+const styles = {
+  imageSize: {
+    width: 100,
+    height: 100
+  }
+};
 
 class Test extends Component {
   state = { shows: [] };
@@ -14,7 +34,31 @@ class Test extends Component {
   }
 
   renderShows() {
-    return this.state.shows.map(show => <Text>{show.name}</Text>);
+    return this.state.shows.map(show => (
+      <View>
+        <Card style={{ elevation: 3 }}>
+          <CardItem>
+            <Left>
+              <Thumbnail source={show.image} />
+              <Body>
+                <Text>{show.name}</Text>
+                <Text note>{show.premiered}</Text>
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem cardBody>
+            <Image
+              style={{ height: 300, flex: 1 }}
+              source={{ uri: show.image.medium }}
+            />
+          </CardItem>
+          <CardItem>
+            <Icon name="heart" style={{ color: '#ED4A6A' }} />
+            <Text>{show.name}</Text>
+          </CardItem>
+        </Card>
+      </View>
+    ));
   }
 
   render() {
