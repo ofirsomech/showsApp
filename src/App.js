dff1649e9122478b03d361337cc54e6e606def83
ofirsomech <ofirsoemch@hotmail.com>
2019-07-08 08:13:01 +0300

    create new App component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World!!</Text>
+      </View>
+    );
+  }
+}
+export default App;
